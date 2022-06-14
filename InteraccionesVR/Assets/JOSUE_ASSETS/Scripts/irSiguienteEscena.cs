32f0a3522b03949604f5c220b5f3e9a52b1c9207
Javier <javier.alfaro.jara@gmail.com>
2022-06-14 10:16:45 -0600

    Orden en script irEscena

--- InteraccionesVR/Assets/JOSUE_ASSETS/Scripts/irSiguienteEscena.cs
+++ InteraccionesVR/Assets/JOSUE_ASSETS/Scripts/irSiguienteEscena.cs
@@ -11,53 +11,64 @@ public class irSiguienteEscena : MonoBehaviour
     {
 
         // Ir a la escena de fuego
-        if (collision.gameObject.name == "irAEscenaDeFuego")
+        if(collision.gameObject.name == "irMenu")
+        {
+            print("De vuelta al menu");
+            SceneManager.LoadScene(0);
+
+        }
+        else if (collision.gameObject.name == "irAEscenaDeFuego")
         {
             print("Vamonossss a la escena de fuego");
             SceneManager.LoadScene(1);
 
-            // Ir a la escena de explicacion cueva
-        } else if (collision.gameObject.name == "Teletrasportador")
+        }
+        else if (collision.gameObject.name == "irAexplicacionFuego")
+        {
+            print("Ir a la escena de la explicacion de fuego");
+            SceneManager.LoadScene(5);
+
+        }
+        else if (collision.gameObject.name == "Teletrasportador")
         {
             print("Ir a la escena explicacion cueva");
             SceneManager.LoadScene(8);
 
-            // Ir a la escena cueva
         } else if (collision.gameObject.name == "irEscenaCueva")
         {
             print("Ir a la escena de la cueva");
             SceneManager.LoadScene(2);
 
-            // Ir a la escena final
-        } else if (collision.gameObject.name == "irEscenaZombie") 
+        }
+        else if (collision.gameObject.name == "irExplicacionZombie")
+        {
+            print("Ir a la escena de la explicaci�n de zombies");
+            SceneManager.LoadScene(7);
+
+        }
+        else if (collision.gameObject.name == "irEscenaZombie") 
         {
             print("Ir a la escena del Zombie");
             SceneManager.LoadScene(3);
 
-        } else if (collision.gameObject.name == "escenaFinal")
+        }
+        else if (collision.gameObject.name == "escenaFinal")
         {
             print("Se pasa el juego y nos vamos a la escena final");
-            //SceneManager.LoadScene(5);
+            SceneManager.LoadScene(4);
 
-
-        } else if (collision.gameObject.name == "salirDelJuego")
+        }
+        else if (collision.gameObject.name == "salirDelJuego")
         {
             print("Se quiere salir del juego");
             Application.Quit();
-        }else if (collision.gameObject.name == "irAexplicacionFuego")
-        {
-            print("Ir a la escena de la explicacion de fuego");
-            SceneManager.LoadScene(5);
 
-        } else if (collision.gameObject.name == "muerte")
+        }
+        else if (collision.gameObject.name == "muerte")
         {
             print("Chale se muri�");
             SceneManager.LoadScene(6);
 
-        } else if (collision.gameObject.name == "irExplicacionZombie")
-        {
-            print("Ir a la escena de la explicaci�n de zombies");
-            SceneManager.LoadScene(7);
-        }
+        } 
     }
 }
